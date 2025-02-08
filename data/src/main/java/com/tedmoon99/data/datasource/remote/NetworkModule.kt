27ffa170bf8e47226cb9a3b5dc832f439328cd16
TedMoon99 <mtj1015@naver.com>
2025-02-08 20:52:26 +0900

    [Feat] NetworkModule.kt 생성 - NetworkModule.provideLoggingInterceptor - NetworkModule.provideOkHttpClient - NetworkModule.provideRetrofit

---
+++ data/src/main/java/com/tedmoon99/data/datasource/remote/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.tedmoon99.data.datasource.remote
+
+import com.tedmoon99.data.BuildConfig.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            // 요청 및 응답 로그 출력
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .addNetworkInterceptor(loggingInterceptor) // 로그 인터셉터
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
