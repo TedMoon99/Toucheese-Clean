ebe224feb78b1f009830f38dec645bb7fa803759
TedMoon99 <mtj1015@naver.com>
2025-02-17 22:57:17 +0900

    [Feat] 화면 연결

--- presentation/src/main/java/com/toucheese/presentation/navigation/ToucheeseNavigation.kt
+++ presentation/src/main/java/com/toucheese/presentation/navigation/ToucheeseNavigation.kt
@@ -1,5 +1,6 @@
 package com.toucheese.presentation.navigation
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -10,17 +11,19 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.toucheese.presentation.ui.screens.HomeScreen
+import com.toucheese.presentation.ui.screens.SignInScreen
 import com.toucheese.presentation.utils.Navigation.Companion.bottomNavClicked
 import com.toucheese.presentation.utils.Screen
 
 @Composable
 fun ToucheeseNavigation() {
     val navController = rememberNavController()
+    val hostState = remember { SnackbarHostState() }
     var bottomNavSelectedTab by remember { mutableIntStateOf(0) }
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route
+        startDestination = Screen.SignIn.route
     ) {
 
         composable(Screen.Home.route){ backStackEntry: NavBackStackEntry ->
@@ -34,6 +37,18 @@ fun ToucheeseNavigation() {
                 },
                 onCardClick = { conceptId: Int ->
                     // 컨셉 스튜디오 조회 페이지로 이동
+
+                }
+            )
+        }
+
+        composable(Screen.SignIn.route){ backStackEntry: NavBackStackEntry ->
+            SignInScreen(
+                hostState = hostState,
+                onSignInClicked = {
+
+                },
+                onKakaoSignInClicked = {
 
                 }
             )
--- presentation/src/main/java/com/toucheese/presentation/utils/Screen.kt
+++ presentation/src/main/java/com/toucheese/presentation/utils/Screen.kt
@@ -3,4 +3,6 @@ package com.toucheese.presentation.utils
 sealed class Screen(val route: String) {
     // Home
     data object Home: Screen("Home")
+    // Sign In
+    data object SignIn: Screen("SignIn")
 }
