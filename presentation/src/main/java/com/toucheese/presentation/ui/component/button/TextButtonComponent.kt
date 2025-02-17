207cc981e80e0ccf86e67221e5d5240c13530eb0
TedMoon99 <mtj1015@naver.com>
2025-02-17 22:56:23 +0900

    [UI] TextButtonComponent.kt.kt 생성

---
+++ presentation/src/main/java/com/toucheese/presentation/ui/component/button/TextButtonComponent.kt
@@ -0,0 +1,22 @@
+package com.toucheese.presentation.ui.component.button
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun TextButtonComponent(
+    @StringRes label: Int,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Text(
+        text = stringResource(label),
+        modifier = modifier.clickable {
+            onClick()
+        }
+    )
+}
